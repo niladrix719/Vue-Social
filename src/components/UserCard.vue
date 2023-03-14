5e816289c5f05d096ac13fb9ad326f69373417ac
Niladri <rickyrickado14@gmail.com>
2023-03-14 18:06:41 +0530

    added a post ux

---
+++ src/components/UserCard.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card">
+    <img src="../assets/dp.jpg" alt="Profile Picture" class="profile-picture">
+    <p class="email">rickyrickado14@gmail.com</p>
+    <h2>Niladri Adhikary</h2>
+    <button class="btn">Show Posts</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+}
+</script>
+
+<style>
+  .card{
+    color: black;
+    height: 20rem;
+    width: 17rem;
+    background-color: white;
+    box-shadow: 0px 0px 20px 10px lightgrey;
+    border-radius: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-sizing: border-box;
+    padding: 1rem;
+    cursor: pointer;
+    transition: 0.25s ease-in-out;
+  }
+
+  .card:hover{
+    transform: scale(1.05);
+  }
+
+  .profile-picture{
+    border-radius: 50%;
+    height: 6.5rem;
+    width: 6.5rem;
+  }
+
+  .email{
+    font-size: 0.8rem;
+    margin-bottom: 0rem;
+  }
+
+  .btn{
+    background-color: #4d5ed3;
+    outline: none;
+    border: none;
+    height: 2.5rem;
+    width: 10rem;
+    color: white;
+    cursor: pointer;
+    margin-top: 2rem;
+    border-radius: 1rem;
+    font-size: 1rem;
+  }
+</style>
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,9 +1,45 @@
 <template>
-  <h1>Users</h1>
+  <div id="users">
+
+    <!-- heading -->
+    <h1 id="user-heading">Welcome to Vue-Social</h1>
+
+    <!-- list of users -->
+    <div id="user-list">
+      <UserCard />
+    </div>
+
+  </div>
 </template>
 
 <script>
+import UserCard from './UserCard'
 export default {
   name: 'UserList',
+  components:{
+    UserCard
+  }
 }
 </script>
+
+<style>
+  #users{
+    width: 75%;
+    margin: auto;
+  }
+
+  #user-list{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  #user-heading{
+    text-align: center;
+    color: #4d5ed3;
+    margin-bottom: 3.5rem;
+    margin-top: 2rem;
+    border-bottom: 1px solid lightgrey;
+    padding-bottom: 2rem;
+  }
+</style>
